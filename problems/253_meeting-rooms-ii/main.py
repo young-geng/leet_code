89060edd45cc4903a642c03249f21921987fdd91
Cem Koc <cemkoc95@gmail.com>
2016-10-23 03:48:17 -0700

    idea is that minheap keeps track of the meeting rooms (resources) availabilities, we add a new interval to heap if first available room still overlaps with it.

--- problems/253_meeting-rooms-ii/main.py
+++ problems/253_meeting-rooms-ii/main.py
@@ -1,27 +1,53 @@
-# https://leetcode.com/problems/balanced-binary-tree/
+# https://leetcode.com/problems/meeting-rooms-ii/
 
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+import heapq
+class MyHeap(object):
+    def __init__(self, initial=None, key=lambda x:x):
+        self.key = key
+        if initial:
+            self._data = [(key(item),item) for item in initial]
+            heapq.heapify(self._data)
+        else:
+            self._data = []
+    def push(self, item):
+        heapq.heappush(self._data, (self.key(item), item))
+
+    def pop(self):
+        return heapq.heappop(self._data)[1]
+
+    def size(self):
+        return len(self._data)
+
 
 class Solution(object):
-    def isBalanced(self, root):
+    def minMeetingRooms(self, intervals):
         """
-        :type root: TreeNode
-        :rtype: bool
+        :type intervals: List[Interval]
+        :rtype: int
         """
-        if not root:
-            return True
-        # get left subtree depth
-        left = self.depth(root.left)
-        # get right subtree depth
-        right = self.depth(root.right)
-        return abs(left - right) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
+        if len(intervals) == 0: return 0
 
-    def depth(self, node):
-        if not node:
-            return 0
-        return 1 + max(self.depth(node.left), self.depth(node.right))
+        pq = MyHeap(key=lambda t: t.end)
+        intervals = sorted(intervals, key=lambda i: i.start)
+        pq.push(intervals[0])
+        for current in intervals[1:]:
+            # get the meeting room that finishes first
+            earliest = pq.pop()
+            # if the current meeting starts right after the earliest finishes,
+            # no need for a new room, just use the same room, so extend the
+            # meeting room timeslot
+            if current.start >= earliest.end:
+                earliest.end = current.end
+            # otherwise the current meeting needs a new room
+            # add new room to heap
+            else:
+                pq.push(current)
+            # put the earliest back to the heap to keep track of that room
+            pq.push(earliest)
+
+        return pq.size()
