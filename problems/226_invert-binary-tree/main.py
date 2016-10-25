929b827cf924395ebb4879cedc4d205b23251cb5
Cem Koc <cemkoc95@gmail.com>
2016-10-24 22:15:34 -0700

    used recursion and in order traversal

---
+++ problems/226_invert-binary-tree/main.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/invert-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root == None: return
+        temp = root.right
+        root.right = self.invertTree(root.left)
+        root.left = self.invertTree(temp)
+        return root
