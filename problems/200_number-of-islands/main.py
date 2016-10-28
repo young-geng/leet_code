1e83542ea9a7d936c244a7acf4adb1e3ddac6bd5
Cem Koc <cemkoc95@gmail.com>
2016-10-28 02:37:09 -0700

    bfs iterative using queue solution added

--- problems/200_number-of-islands/main.py
+++ problems/200_number-of-islands/main.py
@@ -33,9 +33,37 @@ class Solution(object):
         return cc_count
 
 # BFS Solution for practice
-# class AlternativeSolution(object):
-#     def numIslands(self, grid):
-#         """
-#         :type grid: List[List[str]]
-#         :rtype: int
-#         """
+import Queue
+class BFSSolution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if len(grid) == 0: return 0
+
+        cols, rows = len(grid), len(grid[0])
+        cc_count = 0
+        for i in xrange(cols):
+            for j in xrange(rows):
+                if grid[i][j] == '1':
+                    self.bfs(grid, i, j)
+                    cc_count += 1
+        return cc_count
+
+    def bfs(self, grid, i, j):
+        cols, rows = len(grid), len(grid[0])
+        q = Queue.Queue()
+        q.put([i,j))
+        while not q.empty():
+            x, y = q.get()
+            if grid[x][y] == '1':
+                grid[x][y] = '0'
+                self.pushToQueue(q, cols, rows, x-1, y)
+                self.pushToQueue(q, cols, rows, x+1, y)
+                self.pushToQueue(q, cols, rows, x, y-1)
+                self.pushToQueue(q, cols, rows, x, y+1)
+
+    def pushToQueue(self, q, cols, rows, i, j):
+        if i >= 0 and i < cols and j >= 0 and j <= rows:
+            q.put([i,j])
