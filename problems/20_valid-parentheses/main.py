3961e55f7223af661d6792a2a259e1b6c0c51420
Cem Koc <cemkoc95@gmail.com>
2016-10-30 18:09:04 -0700

    solved by iterating thorugh string, pushing the stack when seen an open, popping off when a closed is seen and comparing the last item.

---
+++ problems/20_valid-parentheses/main.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/valid-parentheses/
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if not s:
+            return True
+        stack = []
+
+        for i in xrange(len(s)):
+            # if its opening it, its it getting deeper so add to stack
+            if s[i] in "([{":
+                stack.append(s[i])
+            else:
+                if len(stack) == 0:
+                    return False
+                last = stack.pop()
+                if s[i] == ")" and last != "(": return False
+                if s[i] == "]" and last != "[": return False
+                if s[i] == "}" and last != "{": return False
+        return len(stack) == 0
