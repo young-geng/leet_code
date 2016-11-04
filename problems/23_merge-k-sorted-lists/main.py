2a05e4035784fbc8a765f1a26c3b3c29a477b7ef
Cem Koc <cemkoc95@gmail.com>
2016-11-04 00:41:12 -0700

    comments are added for clarity

--- problems/23_merge-k-sorted-lists/main.py
+++ problems/23_merge-k-sorted-lists/main.py
@@ -9,12 +9,18 @@ class Solution(object):
         pq = []
         for head in lists:
             if head != None:
+                # push all heads of every list into our PriorityQueue
+                # push in a tuple s.t it can be sorted by the first elem of
+                # the tuple
                 heapq.heappush(pq, (head.val, head))
 
         sentinel = ListNode(None)
         curr = sentinel
+        # when there are still things to add in PQ
         while len(pq) > 0:
+            # get the smallest node in PQ
             node = heapq.heappop(pq)[1]
+            # if node has a next node, add it to our PQ to be processed later
             if node.next:
                 heapq.heappush(pq, (node.next.val, node.next))
             curr.next = node
